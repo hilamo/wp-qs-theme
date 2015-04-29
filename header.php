9cffb3a5112e3e09b86d690219df167283cd6d0e
vol4ikman <vol4ikman@gmail.com>
2015-04-29 11:49:25 +0300

    options

--- header.php
+++ header.php
@@ -6,8 +6,7 @@
 		<!-- dns prefetch -->
 		<link href="//www.google-analytics.com" rel="dns-prefetch" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-		<meta name="viewport" content="width=device-width,initial-scale=1.0" />
-		<meta name="description" content="<?php bloginfo('description'); ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon" />
 		<?php wp_head(); ?>
 	</head>
